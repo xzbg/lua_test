e2f210203010299476fd17dbdd16802905697f25
cd_shenhj <cd_shenhj@woobest.com>
2019-01-11 09:21:08 +0800

    快速排序算法

--- Demo/quick_sort.lua
+++ Demo/quick_sort.lua
@@ -1,13 +1,48 @@
 local count = arg[1]
 local array = {}
-math.randomseed(os.clock())
+math.randomseed(arg[2])
 for i = 1, count do
     array[i] = math.random(1, 100)
 end
 print(table.concat(array, ", "))
 
-function quick_sort(startIndex, endIndex, mid, array)
-    
-
-
+function quick_sort(low, high, arr)
+    if low >= high then
+        return
+    end
+    print("\t  1 : " .. table.concat(array, ", ", low, high))
+    local i = low
+    local j = high
+    local mid = arr[i]
+    while i < j do
+        while (i < j and arr[j] >= mid) do
+            j = j - 1
+        end
+        if i < j then
+            local temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            i = i + 1
+        end
+        while (i < j and arr[i] <= mid) do
+            i = i + 1
+        end
+        if i < j then
+            local temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            j = j - 1
+        end
+        print("\t  2 : " .. table.concat(array, ", ", low, high))
+    end
+    print("------------------")
+    if i > low then
+        quick_sort(low, i - 1, arr)
+    end
+    if j < high then
+        quick_sort(j + 1, high, arr)
+    end
+    print("=======================")
 end
+quick_sort(1, #array, array)
+print(table.concat(array, ", "))
--- Test/Test.lua
+++ Test/Test.lua
@@ -1,18 +1,2 @@
--- require("TestObj")
--- local obj = TestObj.New()
--- obj.Register(
---     "id",
---     function(key, old, val)
---         print(key, old, val)
---     end
--- )
--- obj.id = 10
--- obj.sid = 12
-
-require("Shape")
-require("Square")
-local s = Shape:new()
-s:printArea()
-
-local sq = Square:new()
-sq:printArea()
+local a, b = math.modf(arg[1])
+print(a, b)
