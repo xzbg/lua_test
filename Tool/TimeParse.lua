2c50c6b8eb01a9397b793330627dfbc72cbcbc82
cd_shenhj <cd_shenhj@woobest.com>
2019-04-13 15:58:14 +0800

    时间戳解析

--- Tool/TimeParse.lua
+++ Tool/TimeParse.lua
@@ -28,4 +28,45 @@ function ParseDateTime(secondTime, expand)
     end
     return str
 end
-print(ParseDateTime(time, expand))
+
+function TimeFormat1(millSecondTime, expand)
+    local secondTime = millSecondTime / 1000
+    local day = math.floor(secondTime / 86400)
+    local hour = math.floor((secondTime % 86400) / 3600)
+    local min = math.floor((secondTime % 86400 % 3600) / 60)
+    local sec = math.floor(secondTime % 86400 % 3600 % 60)
+    local mill = math.floor(millSecondTime % 1000)
+    local dateTb = {day, hour, min, sec, mill}
+    local index = next(dateTb)
+    while index do
+        if dateTb[index] > 0 then
+            break
+        end
+        index = next(dateTb, index)
+    end
+    local str = ""
+    if index == #dateTb then
+        str = string.format("%d", 0)
+    elseif index == #dateTb - 1 and expand and expand == 1 then
+        str = string.format("%d", sec)
+    else
+        local count = 0
+        for i = 1, #dateTb do
+            if count <= 0 then
+                if dateTb[i] > 0 then
+                    str = str .. string.format("%02d", dateTb[i])
+                    count = count + 1
+                end
+            else
+                str = str .. string.format(":%02d", dateTb[i])
+                count = count + 1
+            end
+            if expand and count >= expand then
+                break
+            end
+        end
+    end
+    return str
+end
+
+print(TimeFormat1(time, expand))
